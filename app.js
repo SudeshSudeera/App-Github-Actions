6c40cf77e37aed5ef28175cd6bbb34da6c7861ab
Sudesh Sudeera <sudeshsudeera@gmail.com>
2024-05-26 17:17:03 +0530

    change message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, resp)=>{
-    resp.status(200).json({message: 'Success...'});
+    resp.status(200).json({message: 'Hello world...Success...'});
 });
 
 app.listen(port, ()=>{
